79422d953366ef8217eb814ab32c1b412e79c17c
Albert Leon <albertleon@Alberts-MacBook-Pro.local>
2019-01-17 20:26:05 -0500

    landing page

---
+++ app-swoosh/appSwoosh/appSwoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appSwoosh
+//
+//  Created by Albert Leon on 1/17/19.
+//  Copyright © 2019 Albert Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
